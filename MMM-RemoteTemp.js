d6b6314394d6fa91b461504842ed331c85a6b9e0
JC-Reno <58151975+JC-Reno@users.noreply.github.com>
2024-11-20 16:22:37 -0800

    2024-11-20 16:22:45

--- MMM-RemoteTemp.js
+++ MMM-RemoteTemp.js
@@ -1,64 +1,27 @@
 Module.register("MMM-RemoteTemp", {
 
-  defaults: {
-    exampleContent: ""
-  },
+  const NodeHelper = require("node_helper");
+  const axios = require("axios");
 
-  /**
-   * Apply the default styles.
-   */
-  getStyles() {
-    return ["remotetemp.css"]
-  },
+  module.exports = NodeHelper.create({
+    start() {
+      console.log("Remote Temperature Helper started.");
+      this.fetchData();
+      setInterval(() => {
+        this.fetchData();
+      }, 60000); // Fetch data every 60 seconds
+    },
 
-  /**
-   * Pseudo-constructor for our module. Initialize stuff here.
-   */
-  start() {
-    this.templateContent = this.config.exampleContent
-
-    // set timeout for next random text
-    setInterval(() => this.addRandomText(), 3000)
-  },
-
-  /**
-   * Handle notifications received by the node helper.
-   * So we can communicate between the node helper and the module.
-   *
-   * @param {string} notification - The notification identifier.
-   * @param {any} payload - The payload data`returned by the node helper.
-   */
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "EXAMPLE_NOTIFICATION") {
-      this.templateContent = `${this.config.exampleContent} ${payload.text}`
-      this.updateDom()
+    fetchData() {
+      const sensorId = this.config.sensorId; // Get sensor ID from config
+      axios.post("http://rpi44-mm:8080/remote-temperature", {
+        sensorId: sensorId,
+      })
+        .then(response => {
+          this.sendSocketNotification("REMOTE_TEMPERATURE_DATA", response.data);
+        })
+        .catch(error => {
+          console.error("Error fetching remote temperature data:", error);
+        });
     }
-  },
-
-  /**
-   * Render the page we're on.
-   */
-  getDom() {
-    const wrapper = document.createElement("div")
-    wrapper.innerHTML = `<b>Title</b><br />${this.templateContent}`
-
-    return wrapper
-  },
-
-  addRandomText() {
-    this.sendSocketNotification("GET_RANDOM_TEXT", { amountCharacters: 15 })
-  },
-
-  /**
-   * This is the place to receive notifications from other modules or the system.
-   *
-   * @param {string} notification The notification ID, it is preferred that it prefixes your module name
-   * @param {number} payload the payload type.
-   */
-  notificationReceived(notification, payload) {
-    if (notification === "TEMPLATE_RANDOM_TEXT") {
-      this.templateContent = `${this.config.exampleContent} ${payload}`
-      this.updateDom()
-    }
-  }
-})
+  });
--- node_helper.js
+++ node_helper.js
@@ -1,12 +1,13 @@
-const NodeHelper = require("node_helper")
+const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
-
-  async socketNotificationReceived(notification, payload) {
-    if (notification === "GET_RANDOM_TEXT") {
-      const amountCharacters = payload.amountCharacters || 10
-      const randomText = Array.from({ length: amountCharacters }, () => String.fromCharCode(Math.floor(Math.random() * 26) + 97)).join("")
-      this.sendSocketNotification("EXAMPLE_NOTIFICATION", { text: randomText })
-    }
+  start() {
+    console.log("Remote Temperature Helper started.");
   },
-})
+
+  socketNotificationReceived(notification, payload) {
+    if (notification === "REMOTE_TEMPERATURE_DATA") {
+      this.sendSocketNotification("REMOTE_TEMPERATURE_DATA", payload);
+    }
+  }
+});
