faf9a48be4dd853956d62d41e36ba78b33583a66
JC-Reno <58151975+JC-Reno@users.noreply.github.com>
2024-12-04 16:26:44 -0800

    2024-12-04 16:26:49

--- example_config.js
+++ example_config.js
@@ -3,9 +3,7 @@
 {
     module: "MMM-RemoteTemp",
         disabled: false,
-            pages: {
-        calendar: "top_right"
-    },
+            pages: { main: "bottom_right", calendar: "top_right", crypto: "top_right" },
     config: {
         sensorId: "office",
             icon: "briefcase",
@@ -13,16 +11,58 @@
     },
     position: "bottom_right"
 },
-
 {
     module: "MMM-RemoteTemp",
         disabled: false,
-            pages: {
-        calendar: "top_right"
-    },
+            pages: { main: "bottom_right", calendar: "top_right", crypto: "top_right" },
     config: {
         sensorId: "guest",
             icon: "restroom",
                 showMore: true
-    }
-
+    },
+    position: "top_right"
+},
+{
+    module: "MMM-RemoteTemp",
+        disabled: false,
+            pages: { main: "bottom_center", calendar: "top_center", crypto: "top_center" },
+    config: {
+        sensorId: "garage",
+            icon: "car",
+                showMore: true
+    },
+    position: "top_center"
+},
+{
+    module: "MMM-RemoteTemp",
+        disabled: false,
+            pages: { main: "bottom_center", calendar: "top_center", crypto: "top_center" },
+    config: {
+        sensorId: "patio",
+            icon: "umbrella-beach",
+                showMore: true
+    },
+    position: "top_center"
+},
+{
+    module: "MMM-RemoteTemp",
+        disabled: false,
+            pages: { main: "bottom_left", calendar: "top_left", crypto: "top_left" },
+    config: {
+        sensorId: "entry",
+            icon: "dungeon",
+                showMore: true
+    },
+    position: "top_left"
+},
+{
+    module: "MMM-RemoteTemp",
+        disabled: false,
+            pages: { main: "bottom_left", calendar: "top_left", crypto: "top_left" },
+    config: {
+        sensorId: "refrigerator",
+            icon: "ice-cream",
+                showMore: true
+    },
+    position: "top_left"
+},
